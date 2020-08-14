677f750205fce831a9f6ad0b6afa622dbe903830
wongcmk <wongcmk@gmail.com>
2020-08-14 22:04:26 +0930

    .gitignore is now working

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("chickenburger");
+INSERT INTO burgers (burger_name) VALUES ("cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("angusbeefburger");
