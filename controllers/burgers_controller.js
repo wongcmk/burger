977f7811dd27cd8837eb535b4fa34dfbdfdefbc4
wongcmk <wongcmk@gmail.com>
2020-08-26 19:26:48 +0930

    including bug fixes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,12 +4,12 @@ var router = express.Router();
 
 var burgers = require("../models/burger.js");
 
-router.get("/", function(req,res){
+router.get("/", function (req, res) {
 	res.redirect("burgers")
 });
 
-router.get("/burgers", function(req,res){
-	burgers.selectAll(function(data){
+router.get("/burgers", function (req, res) {
+	burgers.selectAll(function (data) {
 		var hbsObject = {
 			burgers: data
 		};
@@ -18,23 +18,23 @@ router.get("/burgers", function(req,res){
 	});
 });
 
-router.post("/burgers/create", function(req,res){
-	burgers.insertOne([
+router.post("/burgers/create", function (req, res) {
+	burgers.create([
 		"burger_name"
-		],[
-			req.body.burger_name
-			], function(data){
-				res.redirect("/burgers");
-			});
+	], [
+		req.body.burger_name
+	], function (data) {
+		res.redirect("/burgers");
+	});
 });
 
-router.put("/burgers/update/:id", function(req,res){
+router.put("/burgers/update/:id", function (req, res) {
 	var condition = "id = " + req.params.id;
 	console.log("condition", condition);
 
-	burgers.updateOne({
-		"devoured": req.body.devoured
-	}, condition, function(data){
+	burgers.update({
+		devoured: true
+	}, condition, function (data) {
 		res.redirect("/burgers")
 	});
 });
