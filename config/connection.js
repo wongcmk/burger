96b6821c933e08989109d737d45f0b9359a8dc30
wongcmk <wongcmk@gmail.com>
2020-08-26 18:42:20 +0930

    Installing method override

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ if(process.env.JAWSDB_URL){
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
-		port: 8080,
+		port: 3306,
 		host: "localhost",
 		user: "root",
 		password: "root",
